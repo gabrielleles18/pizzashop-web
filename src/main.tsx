f2d4fdfacf8d7bdd6fc59fd2406dd27251b936a3
gabrielleles18 <gabriel.leles18@gmail.com>
2024-05-11 12:02:32 -0300

    Layout atualizado

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {App} from './App'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { enableMSW } from './api/mocks'
+import { App } from './app'
+
+enableMSW().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App/>
+      <App />
     </React.StrictMode>,
-)
+  )
+})
